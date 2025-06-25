6d424aeb1c618b12ea41066851bfddaf037c6ad9
NathanGRSDesktop <nathangeers00@gmail.com>
2025-06-25 23:46:04 +0200

    Update grid drawing function to remove tile_size parameter and adjust window width

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 # Screen variables
-width = 800
+width = 810
 height = 600
 tile_size = 30
 
@@ -15,27 +15,27 @@ pygame.init()
 # Create a window
 window = pygame.display.set_mode((width, height))
 # Sets the title of the window.
-pygame.display.set_caption('Snake')\
+pygame.display.set_caption('Snake')
 # Load an icon for the window.
 icon = pygame.image.load('ic_snake.png')
 pygame.display.set_icon(icon)
 
-def draw_grid(tile_size):
+def draw_grid():
     # Add a background color
     window.fill(background_color)
 
+    # Vertical lines
     for x in range(tile_size, width, tile_size):
         pygame.draw.line(window, grid_line_color, (x, 0), (x, height))
-
+    # Horizontal lines
     for y in range(tile_size, height, tile_size):
         pygame.draw.line(window, grid_line_color, (0, y), (width, y))
-
 
 # Make sure the window stays on the screen.
 running = True
 while running:
     # Draw the grid
-    draw_grid(tile_size)
+    draw_grid()
 
     for event in pygame.event.get():
         # Check for quit event
