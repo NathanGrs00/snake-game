45597de301eb9af373a916ba6d0573723bb9a38f
NathanGRSDesktop <nathangeers00@gmail.com>
2025-06-25 23:11:41 +0200

    Add initial Pygame setup with window creation

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create a window
+window = pygame.display.set_mode((800, 600))
+# Sets the title of the window.
+pygame.display.set_caption('Snake')
+
+# Make sure the window stays on the screen.
+while True:
+    pass
+
