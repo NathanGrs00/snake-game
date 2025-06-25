66f4e2b9256653d3d56e4df03a698b56ccd6b3ba
NathanGRSDesktop <nathangeers00@gmail.com>
2025-06-25 23:14:01 +0200

    Implement event handling to allow window closure

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Snake')
 
 # Make sure the window stays on the screen.
-while True:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
 
